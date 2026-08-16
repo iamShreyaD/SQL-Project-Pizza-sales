be3463936838d023d9fe454527792a2665fe58ce
Shreya Deshpande <70312732+iamShreyaD@users.noreply.github.com>
2026-08-16 17:42:22 +0530

    Update 3_advanced.sql

--- 3_advanced.sql
+++ 3_advanced.sql
@@ -4,7 +4,8 @@
 
 select 
 	category,
-    pizza_type_id
+    pizza_type_id,
+	revenue
 from (
 select 
 	category,
@@ -25,16 +26,16 @@ where rev <= 3
 
 
 Ans.
-category | pizza_type_id
-Chicken	thai_ckn
-Chicken	bbq_ckn
-Chicken	cali_ckn
-Classic	classic_dlx
-Classic	hawaiian
-Classic	pepperoni
-Supreme	spicy_ital
-Supreme	ital_supr
-Supreme	sicilian
-Veggie	four_cheese
-Veggie	mexicana
-Veggie	five_cheese
+category | pizza_type_id | revenue
+Chicken	thai_ckn	43434.25
+Chicken	bbq_ckn	42768
+Chicken	cali_ckn	41409.5
+Classic	classic_dlx	38180.5
+Classic	hawaiian	32273.25
+Classic	pepperoni	30161.75
+Supreme	spicy_ital	34831.25
+Supreme	ital_supr	33476.75
+Supreme	sicilian	30940.5
+Veggie	four_cheese	32265.7
+Veggie	mexicana	26780.75
+Veggie	five_cheese	26066.5
