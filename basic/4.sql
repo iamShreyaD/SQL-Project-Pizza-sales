1782acfdd0eaa592c59b80f49f167198e07d6d16
Shreya Deshpande <70312732+iamShreyaD@users.noreply.github.com>
2026-08-20 22:55:53 +0530

    Create 4.sql

---
+++ basic/4.sql
@@ -0,0 +1,17 @@
+
+-- Identify the most common pizza size ordered.
+
+select p.size as pizza_size, count(od.order_details_id) as no_of_pizza
+from order_details as od
+join pizzas as p on od.pizza_id = p.pizza_id
+group by p.size
+order by no_of_pizza desc;
+
+
+Ans.
+pizza_size | no_of_pizza
+L          | 18526
+M          | 15385
+S          | 14137
+XL         | 544
+XXL        | 28
