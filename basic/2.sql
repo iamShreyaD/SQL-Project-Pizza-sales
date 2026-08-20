f015eaa2371cdc3cb6b6a1a5a0a7894a8e8f15d8
Shreya Deshpande <70312732+iamShreyaD@users.noreply.github.com>
2026-08-20 22:54:18 +0530

    Create 2.sql

---
+++ basic/2.sql
@@ -0,0 +1,8 @@
+-- Calculate the total revenue generated from pizza sales.
+
+SELECT ROUND(SUM(od.quantity * p.price), 2) AS total_sales
+FROM order_details AS od
+JOIN pizzas AS p ON od.pizza_id = p.pizza_id;
+
+
+Ans. total_sales = 817860.05
